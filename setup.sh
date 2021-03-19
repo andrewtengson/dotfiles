c2aff17b20c418c6def0cc66030a9c0b8ed531c8
Andrew Tengson <andrew.tengson@paymaya.com>
2021-03-19 20:47:16 +0800

    Added setup.sh

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -eao pipefail
+
+# dotfiles
+create_symlink() {
+  file=$1
+  src="${PWD}/${file}"
+  dst="${HOME}/${file}"
+
+  if ! [[ -f "${dst}" ]]; then
+    if ! [[ -d "$(dirname "${dst}")" ]]; then
+      mkdir -p "$(dirname "${dst}")"
+    fi
+
+    echo "Creating ${dst} symlink..."
+    ln -sfn "${src}" "${dst}"
+  fi
+}
+
+dotfiles=(
+  ".zshrc"
+  ".tmux.conf"
+  ".oh-my-zsh/custom/themes/agnoster-mod.zsh-theme"
+  ".config/nvim/coc-settings.json"
+  ".config/nvim/init.vim"
+  ".config/aliasrc"
+)
+
+for file in "${dotfiles[@]}"; do
+  create_symlink "${file}"
+done
+
+ln -sfn "${HOME}/.config/nvim/init.vim" "${HOME}/.vimrc"
+
+# zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
