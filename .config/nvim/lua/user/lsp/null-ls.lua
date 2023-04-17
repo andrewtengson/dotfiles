f444f54216ab37d29c2413600747994f7d640fc0
Andrew Tengson <andrew.tengson@paymaya.com>
2023-04-17 19:45:41 +0800

    Add rustfmt to null-ls

--- .config/nvim/lua/user/lsp/null-ls.lua
+++ .config/nvim/lua/user/lsp/null-ls.lua
@@ -24,6 +24,7 @@ null_ls.setup({
     formatting.golines,
     formatting.google_java_format,
     formatting.yapf,
+    formatting.rustfmt,
 
     diagnostics.write_good,
     diagnostics.shellcheck,
