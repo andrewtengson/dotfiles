ec8552656492b3a7bb3629d661070122cf7d490f
Andrew Tengson <andrew.tengson@paymaya.com>
2023-04-30 19:47:45 +0800

    Add default handlers

--- .config/nvim/lua/user/dap/mason-nvim-dap.lua
+++ .config/nvim/lua/user/dap/mason-nvim-dap.lua
@@ -13,6 +13,9 @@ local sources = {
 mason_nvim_dap.setup({
   ensure_installed = sources,
   automatic_setup = true,
+  handlers = {
+    function(config)
+      mason_nvim_dap.default_setup(config)
+    end,
+  },
 })
-
-mason_nvim_dap.setup_handlers({})
