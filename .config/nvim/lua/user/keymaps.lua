176ca2bda25d37d684cd22a31e55bcb04f3291f8
Andrew Tengson <andrew.tengson@paymaya.com>
2023-06-08 12:07:11 +0800

    Update command for telescope find file

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -97,12 +97,7 @@ keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
 keymap("n", "<leader>F", ":NvimTreeFindFile<cr>", opts)
 
 -- Telescope
-keymap(
-  "n",
-  "<leader>f",
-  "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>",
-  opts
-)
+keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files()<cr>", opts)
 keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
 keymap("n", "<leader>p", "<cmd> Telescope projects<cr>", opts)
 keymap("n", "<leader>dt", "<cmd> Telescope dap commands<cr>", opts)
