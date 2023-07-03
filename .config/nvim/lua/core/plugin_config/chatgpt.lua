a3b76bde3bcbf7dd4cdc412eedef56654bd72c90
Andrew Tengson <andrew.tengson@paymaya.com>
2023-07-03 15:49:13 +0800

    Override openai params

--- .config/nvim/lua/core/plugin_config/chatgpt.lua
+++ .config/nvim/lua/core/plugin_config/chatgpt.lua
@@ -1,3 +1,12 @@
 require("chatgpt").setup({
   api_key_cmd = "pass openai/chatgpt.nvim/token",
+  openai_params = {
+    model = "gpt-3.5-turbo",
+    frequency_penalty = 0,
+    presence_penalty = 0,
+    max_tokens = 2048,
+    temperature = 0,
+    top_p = 1,
+    n = 1,
+  },
 })
