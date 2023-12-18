24057a523f9cd5ed7419d5fb0f6d3d0fc7a319e0
Andrew Tengson <andrew.tengson@paymaya.com>
2023-12-18 13:29:41 +0800

    Set treesitter sync install to true

--- .config/nvim/lua/core/plugin_config/treesitter.lua
+++ .config/nvim/lua/core/plugin_config/treesitter.lua
@@ -39,14 +39,14 @@ configs.setup({
     "vim",
     "yaml",
   },
-  sync_install = false,
+  sync_install = true,
   ignore_install = { "" }, -- List of parsers to ignore installing
   autopairs = {
     enable = true,
   },
   highlight = {
     enable = true, -- false will disable the whole extension
-    disable = {},  -- list of language that will be disabled
+    disable = {}, -- list of language that will be disabled
     additional_vim_regex_highlighting = false,
   },
   indent = { enable = true, disable = { "yaml" } },
