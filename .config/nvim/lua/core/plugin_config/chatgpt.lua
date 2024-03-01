3e70f848fe7a8a2c081fd8bb0ba21685c51cddd1
Andrew Tengson <andrew.tengson@paymaya.com>
2024-03-01 13:29:16 +0800

    Update max tokens

--- .config/nvim/lua/core/plugin_config/chatgpt.lua
+++ .config/nvim/lua/core/plugin_config/chatgpt.lua
@@ -4,7 +4,7 @@ require("chatgpt").setup({
     model = "gpt-3.5-turbo",
     frequency_penalty = 0,
     presence_penalty = 0,
-    max_tokens = 2048,
+    max_tokens = 8192,
     temperature = 0,
     top_p = 1,
     n = 1,
