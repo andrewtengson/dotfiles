862d55892d3ecf649923c2cc84ffae8fc8e105f6
Andrew Tengson <andrew.tengson@paymaya.com>
2024-07-11 16:19:15 +0800

    feat: disable molten output wrap

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -221,7 +221,7 @@ local plugins = {
     build = ":UpdateRemotePlugins",
     init = function()
       vim.g.molten_auto_open_output = false
-      vim.g.molten_wrap_output = true
+      vim.g.molten_wrap_output = false
       vim.g.molten_virt_text_output = true
       vim.g.molten_virt_lines_off_by_1 = true
     end,
