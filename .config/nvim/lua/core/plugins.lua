f0370899cb03467c63ed5df6ae9f7bc13ed18045
Andrew Tengson <andrew.tengson@paymaya.com>
2025-02-19 21:19:28 +0800

    feat: load avante on cmd

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -395,40 +395,14 @@ local plugins = {
   -- Avante
   {
     "yetone/avante.nvim",
-    event = "VeryLazy",
-    lazy = false,
     version = false,
     build = "make",
-    opts = {
-      provider = "openai",
-      openai = {
-        endpoint = "https://api.openai.com/v1",
-        api_key_name = "cmd:pass openai/token",
-        model = "o1-mini",
-        timeout = 30000,
-        temperature = 0,
-        max_tokens = 8000,
-        disable_tools = true,
-      },
-      claude = {
-        endpoint = "https://api.anthropic.com",
-        api_key_name = "cmd:pass anthropic/token",
-        model = "claude-3-5-sonnet-20241022",
-        timeout = 30000,
-        temperature = 0,
-        max_tokens = 8000,
-      },
-      vendors = {
-        deepseek = {
-          __inherited_from = "openai",
-          api_key_name = "cmd:pass deepseek/token",
-          endpoint = "https://api.deepseek.com",
-          model = "deepseek-reasoner",
-        },
-      },
-      hints = {
-        enabled = false,
-      },
+    cmd = {
+      "AvanteAsk",
+      "AvanteChat",
+      "AvanteEdit",
+      "AvanteToggle",
+      "AvanteFocus",
     },
     dependencies = {
       "stevearc/dressing.nvim",
@@ -461,6 +435,9 @@ local plugins = {
         ft = { "markdown", "Avante" },
       },
     },
+    config = function()
+      require("core.plugin_config.avante")
+    end,
   },
 }
 
