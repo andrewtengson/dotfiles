96907e86d2838aa9dec9bcfa4862ed44ed6cdcca
Andrew Tengson <andrewtengson@protonmail.com>
2026-05-09 17:30:51 +0800

    fix: update deprecated nvim diagnostic jumps

--- .config/nvim/lua/core/plugin_config/lsp/handlers.lua
+++ .config/nvim/lua/core/plugin_config/lsp/handlers.lua
@@ -40,6 +40,12 @@ end
 
 local function lsp_keymaps(bufnr)
   local opts = { noremap = true, silent = true }
+  local diagnostic_jump_opts = {
+    on_jump = function()
+      vim.diagnostic.open_float()
+    end,
+  }
+
   vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "K", '<cmd>lua vim.lsp.buf.hover({ border = "single" })<CR>', opts)
@@ -48,9 +54,13 @@ local function lsp_keymaps(bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "single" })<CR>', opts)
+  vim.keymap.set("n", "[d", function()
+    vim.diagnostic.jump(vim.tbl_extend("force", diagnostic_jump_opts, { count = -1 }))
+  end, vim.tbl_extend("force", opts, { buffer = bufnr }))
   vim.api.nvim_buf_set_keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "single" })<CR>', opts)
+  vim.keymap.set("n", "]d", function()
+    vim.diagnostic.jump(vim.tbl_extend("force", diagnostic_jump_opts, { count = 1 }))
+  end, vim.tbl_extend("force", opts, { buffer = bufnr }))
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>i", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
   vim.cmd([[ command! Format execute 'lua vim.lsp.buf.format()' ]])
 end
